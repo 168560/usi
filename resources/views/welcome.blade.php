4e14f9561abbabfe1099bc0e0cc6e5d1aa4cb977
Maciej Kozera <mmkozera@gmail.com>
2017-05-17 18:38:17 +0200

    widok wiadomosc

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Hello world</div>
+                <div class="title">Projekt USI 2017 IwB</div>
             </div>
         </div>
     </body>
