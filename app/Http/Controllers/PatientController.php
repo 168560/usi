4a0d0124808a429fcd4aa5310fb1bc7990de3021
Maciej Kozera <mmkozera@gmail.com>
2017-05-17 18:36:45 +0200

    importy i klasy kontrolerow

--- app/Http/Controllers/PatientController.php
+++ app/Http/Controllers/PatientController.php
@@ -1,8 +1,12 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+use App\Patient;
+use App\Http\Requests;
+
+class PatientController extends Controller
+{
+    
+}
