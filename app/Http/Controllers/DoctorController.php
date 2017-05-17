6e7dcb45a8d7f40c86e007990752ef2d06148810
Maciej Kozera <mmkozera@gmail.com>
2017-05-17 19:10:40 +0200

    uslugi pobierajace dane

--- app/Http/Controllers/DoctorController.php
+++ app/Http/Controllers/DoctorController.php
@@ -24,4 +24,20 @@ class DoctorController extends Controller
         $appointments = $doctor->appointments;
         return response()->json($appointments);
     }
+    public function delete(){
+        $id = request()->route("id");
+        Doctor::destroy($id);
+        return response()->json(array('id' => $id));
+    }
+    public function getAppointment(){
+        $id = request()->route("id");
+        $id2 = request()->route("id2");
+        $result = Appointment::where(array(array('id', intval($id2)),array('DOCTOR_id', intval($id))))->get()[0];
+        return response()->json($result);
+    }
+    public function getBySpeciality(){
+        $id = request()->route("id");
+        $result = Doctor::where(array(array('SPECIALITY_id', intval($id))))->get();
+        return response()->json($result);
+    }
 }
